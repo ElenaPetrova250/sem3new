71dffef63f8cdc5d3e960f9ad926724b31d65f77
Elena Petrova <elena.i.11@yandex.ru>
2022-08-16 14:18:09 +0400

    task19

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int number = Console.ReadLine();
+int len = number.length();
+//где-то тут я накосячила, но мне по хорошему надо вместе все прорешать, я эти палиндромы и синтаксис для его решения незнаю
+for(int i = 0; i < len/2; ++i);
+		if(number[i] != number[len-i-1])
+		Console.WriteLine($"Ваше число:  - не палиндром.");
+	Console.WriteLine($"Ваше число: - палиндром.");
+breack;
